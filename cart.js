9b8a736f641ec6208d72f1db5f004e9d2179811d
julietaramos <julieta.ramos.jr@gmail.com>
2022-10-25 09:20:58 -0300

    ahora anda el fetch bien

--- cart.js
+++ cart.js
@@ -1,40 +1,60 @@
-import{onLoadCartNumbers} from "./home.js";
 
-function totalCost(product){
-    let cartCost = localStorage.getItem('totalCost');
-    if(cartCost != null){
-      cartCost = parseInt(cartCost);
-      localStorage.setItem("totalCost", cartCost + product.price);
-    } else {
-    localStorage.setItem("totalCost", product.price);
-    }
+function onLoadCartNumbersCart() {
+  let productNumbers = localStorage.getItem('cartNumbers');
+  productNumbers != null && (document.querySelector('.cart span').textContent = productNumbers);
 }
+ 
+let cartItems=localStorage.getItem("productsInCart") || [];
+ cartItems = JSON.parse(cartItems)
 
 function displayCart(){
- let cartItems = localStorage.getItem("productsInCart");
- cartItems = JSON.parse(cartItems); 
  let productContainer = document.querySelector(".products");
  let cartCost = localStorage.getItem('totalCost');
- if(cartItems && productContainer){
-        productContainer.innerHTML = ' ';
-        Object.values(cartItems).map(item => {
-            productContainer.innerHTML += `
+ for(const item of cartItems){
+    const {tag, name, price, inCart, id} = item;
+         const itemHTML = `
+          
+           
+                     <button class="delete-cart cart${id}" >ELIMINAR</button> 
+            <button class="add-product cart${id}" >AGREGAR</button> 
             <div class="product">
-              <ion-icon name="close-circle"></ion-icon>
-              <img src="./image/${item.tag}.jpg"> </img>
-              <span>${item.name}</span>
+           
+            <div class="product-reference">
+              <img src="./image/${tag}.jpg"> </img>
+              <span>${name}</span>
+              
               </div>
-              <div class="price"> ${item.price}</div>
+      
+
+              <div class="price"> ${price}</div>
               <div class="quantity"> 
-              <ion-icon class="decrease" name="arrow-dropleft-circle"> </ion-icon>
-              <span> ${item.inCart} </span>
-              <ion-icon class="increase" name="arrow-dropright-circle"> </ion-icon>
+           
+              <span> ${inCart} </span>
+             
               </div>
               <div class="total">
-              $${item.inCart * item.price},00
+              $${inCart * price},00
               </div>
-            `;
-        });
+              </div>
+          
+            `
+            productContainer.innerHTML += itemHTML;
+ }
+  
+     let deleteProduct = document.querySelectorAll('.delete-cart');
+     for(let i = 0; i< deleteProduct.length; i++){
+           deleteProduct[i].addEventListener('click',()=>{
+             deleteProd(inCart[i]);
+            })
+
+     }
+
+     let addProduct = document.querySelectorAll('.add-product')
+     for (let i= 0; i < addProduct.length; i++){
+        addProduct[i].addEventListener('click',()=>{
+            cartNumbersCart(products[i]);
+            })
+    } 
 
         productContainer.innerHTML += `
         <div class="basketTotalContainer">
@@ -43,12 +63,133 @@ function displayCart(){
                  </h4>
                  <h4 class="basketTotal">
                  $ ${cartCost},00
-        `;
-}
+                 </h4>
+        <button onclick="JSalert()">COMPRAR</button>
+        </div>
+     
+           `;
+
+           onLoadCartNumbersCart(); 
+    }
+displayCart();
+
+
+
+
+
+function JSalert(){
+	Swal.fire({  title: "Require Email!",   
+    text: "Enter your email address:",   
+    type: "input",   
+    showCancelButton: true,   
+    closeOnConfirm: false,   
+    animation: "slide-from-top",   
+    inputPlaceholder: "Your Email address" }, 
+    
+    function(inputValue){   
+        if (inputValue === false) 
+        return false;      
+           if (inputValue === "") {     
+            swal.showInputError("Please enter email!");     
+            return false   
+            }      
+         swal("Action Saved!", "You entered following email: " + inputValue, "success"); });
+         //deleteProducts();
 }
 
-onLoadCartNumbers();
-fetchProducts().then(productsJason => {
-    products = productsJason;
-    showProducts();
-})
+
+
+
+
+
+
+/*PARA ELIMINAR PRODUCTO*/
+
+function deleteProd(producto){
+
+  let prod= Array.from(cartItems).find((p) => p.id === producto.id)
+    if (prod.inCart === 1) {
+        carrito.splice(carrito.findIndex(p => p.id == producto.id), 1)
+    } else {
+        prod.inCart--
+    }
+    ReduceItems(producto);
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems))
+    decreceTotalCost(producto)
+    displayCart();
+}
+
+function cartNumbersReduce(){
+
+    let productNumbers = localStorage.getItem('cartNumbers');
+    //El tipo que trae es string//
+    productNumbers= parseInt(productNumbers);
+    if(productNumbers != 0 ){
+       localStorage.setItem('cartNumbers', productNumbers - 1);
+       document.querySelector('.cart span').textContent = productNumbers -1;
+    } else {
+       localStorage.setItem('cartNumbers', 0);
+       document.querySelector('.cart span').textContent = productNumbers;
+    }
+   
+   }
+
+
+function decreceTotalCost(product){
+    let cartCost = localStorage.getItem('totalCost');
+      cartCost = parseInt(cartCost);
+      localStorage.setItem("totalCost", cartCost - product.price);
+    } 
+
+  /*PARA AGREGAR PRODUCTO*/
+
+
+function setItemsCart(p){
+    const prod = products.find(prod => prod.id == p.id )
+    if (cartItems.find (prod => prod.id== p.id)){
+        const prod = cartItems.find(prod => prod.id == p.id)
+        prod.inCart++
+    } else {
+        cartItems.push({
+            ...prod,
+            inCart: 1
+        })
+        
+    }
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems))
+    totalCostCart(p[i]);
+    displayCart();
+} 
+
+  
+
+function cartNumbersCart(product) {
+
+  let productNumbers = localStorage.getItem('cartNumbers');
+  productNumbers = parseInt(productNumbers);
+  if (productNumbers) {
+      localStorage.setItem('cartNumbers', productNumbers + 1);
+      document.querySelector('.cart span').textContent = productNumbers + 1;
+  } else {
+      localStorage.setItem('cartNumbers', 1);
+      document.querySelector('.cart span').textContent = productNumbers;
+  }
+  setItems(product);
+}
+
+
+ function totalCostCart(product){
+    let cartCost = localStorage.getItem('totalCost');
+    if(cartCost != null){
+      cartCost = parseInt(cartCost);
+      localStorage.setItem("totalCost", cartCost + product.price);
+    } else {
+    localStorage.setItem("totalCost", product.price);
+    }
+}
+  
+
+
+
+
+
--- home.js
+++ home.js
@@ -1,26 +1,18 @@
-let addCarts = document.querySelectorAll('.add-cart');
+
+
+
 
 let number = 0;
 
-class Product{
-    constructor(id,name,precio){
-        this.id = id;
-        this.name = name.toUpperCase();
-        this.price= parseFloat(precio);
-        this.inCart = parseInt (0);
-        
-    }
-}
-
-async function fetchProducts(){
+async function fetchProducts() {
     let response = await fetch('./data.json')
     return await response.json()
 }
 
-let stock =[];
+let products = [];
 
-fetchProducts().then(products => {
-    stock = products
+fetchProducts().then(stock => {
+    products = stock
     showProducts()
 })
 
@@ -28,101 +20,85 @@ fetchProducts().then(products => {
 
 const seccionProducts = document.querySelector('.container')
 
-function showProducts(){
-    for( product of stock){
-        const {tag, name, price, inCart,id} = product;
+function showProducts() {
+    for (const product of products) {
+        const { tag, name, price, inCart, id } = product;
         const productHTML = `
         <div class="image">
         <img src="image/${tag}.jpg"> </img>
         <h3> ${name}</h3>
         <h3>$${price}0</h3>
-        <a class="add-cart cart${id}" href="#">add Cart</a> 
+        <button class="add-cart cart${id}" >add Cart</button> 
         </div>
         `
-        seccionProducts.innerHTML += productHTML
+        seccionProducts.innerHTML += productHTML;
+
     }
-}
 
-showProducts();
-
-
-
-
-function totalCost(product){
-    let cartCost = localStorage.getItem('totalCost');
-    if(cartCost != null){
-      cartCost = parseInt(cartCost);
-      localStorage.setItem("totalCost", cartCost + product.price);
-    } else {
-    localStorage.setItem("totalCost", product.price);
-    }
-}
-
-
-let cartItems = JSON.parse(localStorage.getItem('productsInCart')) || [];
-
-
-
-function setItems(id) {
-
-    const product = stock.find(product => product.id = id);
-    if(cartItems.find(product = product.id == id)){
-        const product = cartItems.find(product => product.id == id)
-            product.cant++
-    } else {
-         cartItems.push({
-            ...product,
-            cant: 1
-         })
-    }
-        localStorage.setItem("productsInCart", JSON.stringify(cartItems));
-        //Uso el stingify para no pasarlo como objeto. 
-}
-
-
-
-function cartNumbers(product){
-
- let productNumbers = localStorage.getItem('cartNumbers');
- //El tipo que trae es string//
- productNumbers= parseInt(productNumbers);
- if(productNumbers){
-    localStorage.setItem('cartNumbers', productNumbers + 1);
-    document.querySelector('.cart span').textContent = productNumbers +1;
- } else {
-    localStorage.setItem('cartNumbers', 1);
-    document.querySelector('.cart span').textContent = productNumbers;
- }
- setItems(product.id);
-}
-
-
-
-/*TIENE LO QUE PIDE LA CLASE 12*/
-
-function onLoadCartNumbers(){
-    let productNumbers = localStorage.getItem('cartNumbers');
-    productNumbers!=null  && (document.querySelector('.cart span').textContent = productNumbers);
-}
-
-
-
-
-for (let i= 0; i < addCarts.length; i++){
-    addCarts[i].addEventListener('click',()=>{
-        cartNumbers(stock[i]);
-        Toastify({
-            text: "Agregaste " + stock[i].name,
-            duration: 3000,
-            gravity: 'bottom',
-            position: 'left',
-            style: {
-                background: 'rgba(253, 108, 122, 1)'
-            }
-    
-        }).showToast();
-    
+    let addCarts = document.querySelectorAll('.add-cart');
+    for (let i = 0; i < addCarts.length; i++) {
+        addCarts[i].addEventListener('click', () => {
+            cartNumbers(products[i]);
+            totalCost(products[i]);
         })
+    }
+    onLoadCartNumbers();
+}
+
+//showProducts();
+const cartItems= JSON.parse(localStorage.getItem('productsInCart')) || [];
+function setItems(p){
+    const prod = products.find(prod => prod.id == p.id )
+    if (cartItems.find (prod => prod.id== p.id)){
+        const prod = cartItems.find(prod => prod.id == p.id)
+        prod.inCart++
+    } else {
+        cartItems.push({
+            ...prod,
+            inCart: 1
+        })
+        
+    }
+
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+} 
+
+
+
+
+
+function cartNumbers(product) {
+
+    let productNumbers = localStorage.getItem('cartNumbers');
+    //El tipo que trae es string//
+    productNumbers = parseInt(productNumbers);
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+    setItems(product);
+}
+
+
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers != null && (document.querySelector('.cart span').textContent = productNumbers);
+}
+
+
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost) + product.price;
+        localStorage.setItem("totalCost", cartCost);
+    } else {
+        localStorage.setItem("totalCost", product.price);
+    }
 }
 
 onLoadCartNumbers();
